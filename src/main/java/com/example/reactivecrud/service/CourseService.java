9610f648e022b23b70aa8e0ca168a8f587b7f230
Tahiru Fuseini <tahirufu@stlghana.com>
2023-01-12 12:28:04 +0000

    service

--- src/main/java/com/example/reactivecrud/service/CourseService.java
+++ src/main/java/com/example/reactivecrud/service/CourseService.java
@@ -1,2 +1,22 @@
-package com.example.reactivecrud.service;public interface CourseService {
+package com.example.reactivecrud.service;
+
+import com.example.reactivecrud.model.Course;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+public interface CourseService {
+
+    Flux<Course> getAllCourses();
+
+    Mono<Course> getCourseById(String id);
+
+    Flux<Course> getCoursesByCategory(String category);
+
+    Mono<Course> saveCourse(Course course);
+
+    Mono<Course> updateCourse(String id, Course course);
+
+    Mono<Void> deleteCourse(String id);
+
+    Mono<Void> deleteAllCourses();
 }
