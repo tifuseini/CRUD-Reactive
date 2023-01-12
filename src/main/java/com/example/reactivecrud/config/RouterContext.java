b84939984246111911f5821f49b21e45bb75ef69
Tahiru Fuseini <tahirufu@stlghana.com>
2023-01-12 15:01:49 +0000

    Router update

--- src/main/java/com/example/reactivecrud/config/RouterContext.java
+++ src/main/java/com/example/reactivecrud/config/RouterContext.java
@@ -7,8 +7,7 @@ import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
-import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
-import static org.springframework.web.reactive.function.server.RequestPredicates.accept;
+import static org.springframework.web.reactive.function.server.RequestPredicates.*;
 import static org.springframework.web.reactive.function.server.RouterFunctions.route;
 
 @Configuration
@@ -17,8 +16,24 @@ public class RouterContext {
     @Bean
     RouterFunction<ServerResponse> routes(CourseHandler courseHandler) {
         return route(GET("/courses")
-                .and(accept(MediaType.APPLICATION_JSON)),
-                courseHandler::findAllCourses);
+                        .and(accept(MediaType.APPLICATION_JSON)),
+                        courseHandler::findAllCourses)
+                .andRoute(GET("/courses/{id}")
+                        .and(accept(MediaType.APPLICATION_JSON)),
+                        courseHandler::findCourseById)
+                .andRoute(POST("/courses")
+                        .and(accept(MediaType.APPLICATION_JSON)),
+                        courseHandler::createCourse)
+                .andRoute(PUT("/courses/{id}")
+                        .and(accept(MediaType.APPLICATION_JSON)),
+                        courseHandler::updateCourse)
+                .andRoute(DELETE("/courses/{id}")
+                        .and(accept(MediaType.APPLICATION_JSON)),
+                        courseHandler::deleteCourse)
+                .andRoute(DELETE("/courses")
+                        .and(accept(MediaType.APPLICATION_JSON)),
+                        courseHandler::deleteAllCourses);
+
     }
 
 
