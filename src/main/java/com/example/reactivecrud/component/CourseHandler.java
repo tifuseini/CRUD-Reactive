19a38186ee8166d8a8df20fcf8dd11cd66743187
Tahiru Fuseini <tahirufu@stlghana.com>
2023-01-12 14:36:27 +0000

    Update

--- src/main/java/com/example/reactivecrud/component/CourseHandler.java
+++ src/main/java/com/example/reactivecrud/component/CourseHandler.java
@@ -1,12 +1,24 @@
 package com.example.reactivecrud.component;
 
+import com.example.reactivecrud.model.Course;
 import com.example.reactivecrud.service.CourseService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
 
 @Component
 @RequiredArgsConstructor
 public class CourseHandler {
 
     private final CourseService courseService;
+
+    public Mono<ServerResponse> findAllCourses(ServerRequest request) {
+        var courses = courseService.getAllCourses();
+        return ServerResponse.ok()
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(courses, Course.class);
+    }
 }
--- src/main/java/com/example/reactivecrud/config/RouterContext.java
+++ src/main/java/com/example/reactivecrud/config/RouterContext.java
@@ -3,14 +3,23 @@ package com.example.reactivecrud.config;
 import com.example.reactivecrud.component.CourseHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.ServerResponse;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+import static org.springframework.web.reactive.function.server.RequestPredicates.accept;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
 
 @Configuration
 public class RouterContext {
 
     @Bean
     RouterFunction<ServerResponse> routes(CourseHandler courseHandler) {
-        return routes();
+        return route(GET("/courses")
+                .and(accept(MediaType.APPLICATION_JSON)),
+                courseHandler::findAllCourses);
     }
+
+
 }
