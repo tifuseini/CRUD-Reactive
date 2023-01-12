220941755b7d0fc5fbbd7c15f63702411c8172ee
Tahiru Fuseini <tahirufu@stlghana.com>
2023-01-12 11:37:42 +0000

    Course POJO

--- src/main/java/com/example/reactivecrud/model/Course.java
+++ src/main/java/com/example/reactivecrud/model/Course.java
@@ -1,4 +1,27 @@
 package com.example.reactivecrud.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Builder
+@Document
+@AllArgsConstructor
+@NoArgsConstructor
 public class Course {
+
+    @Id
+    private String id;
+
+    private String name;
+
+    private String category;
+
+    private String rating;
+
+    private String description;
 }
