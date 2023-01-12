bb645db083301c347c8eea8db57ab4057f7bba9f
Tahiru Fuseini <tahirufu@stlghana.com>
2023-01-12 12:28:34 +0000

    service impl

--- src/main/java/com/example/reactivecrud/serviceImpl/CourseServiceImpl.java
+++ src/main/java/com/example/reactivecrud/serviceImpl/CourseServiceImpl.java
@@ -1,2 +1,58 @@
-package com.example.reactivecrud.serviceImpl;public class CourseServiceImpl {
+package com.example.reactivecrud.serviceImpl;
+
+import com.example.reactivecrud.model.Course;
+import com.example.reactivecrud.repository.CourseRepository;
+import com.example.reactivecrud.service.CourseService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Service
+@RequiredArgsConstructor
+public class CourseServiceImpl implements CourseService {
+
+    private final CourseRepository courseRepository;
+
+    @Override
+    public Flux<Course> getAllCourses() {
+        return courseRepository.findAll();
+    }
+
+    @Override
+    public Mono<Course> getCourseById(String id) {
+        return courseRepository.findById(id);
+    }
+
+    @Override
+    public Flux<Course> getCoursesByCategory(String category) {
+        return courseRepository.findAllByCategory(category);
+    }
+
+    @Override
+    public Mono<Course> saveCourse(Course course) {
+        return courseRepository.save(course);
+    }
+
+    @Override
+    public Mono<Course> updateCourse(String id, Course course) {
+        return courseRepository.findById(id)
+                .flatMap(existingCourse -> {
+                    existingCourse.setName(course.getName());
+                    existingCourse.setCategory(course.getCategory());
+                    existingCourse.setRating(course.getRating());
+                    existingCourse.setDescription(course.getDescription());
+                    return courseRepository.save(existingCourse);
+                });
+    }
+
+    @Override
+    public Mono<Void> deleteCourse(String id) {
+        return courseRepository.deleteById(id);
+    }
+
+    @Override
+    public Mono<Void> deleteAllCourses() {
+        return courseRepository.deleteAll();
+    }
 }
