544aeda14959e0fd1c89f17fff0c7509db84d2ed
Tahiru Fuseini <tahirufu@stlghana.com>
2023-01-12 11:40:01 +0000

    Course Repository POJO

--- src/main/java/com/example/reactivecrud/repository/CourseRepository.java
+++ src/main/java/com/example/reactivecrud/repository/CourseRepository.java
@@ -1,2 +1,13 @@
-package com.example.reactivecrud.repository;public interface CourseRepository {
+package com.example.reactivecrud.repository;
+
+import com.example.reactivecrud.model.Course;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Flux;
+
+@Repository
+public interface CourseRepository extends ReactiveMongoRepository<Course, String> {
+
+    Flux<Course> findAllByCategory(String category);
+
 }
