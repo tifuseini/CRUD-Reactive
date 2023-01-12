beddaec90178efd412925212dfaf183b2f6fcf00
Tahiru Fuseini <tahirufu@stlghana.com>
2023-01-12 11:33:03 +0000

    update

---
+++ src/main/java/com/example/reactivecrud/CrudReactiveApplication.java
@@ -0,0 +1,13 @@
+package com.example.reactivecrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudReactiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudReactiveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/reactivecrud/CrudReactiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reactivecrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudReactiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
